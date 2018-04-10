f1195e22bf747be875af27a7cc3c8937908656d5
Ikey Doherty <ikey@solus-project.com>
2018-04-10 11:49:53 +0100

    scripts: Add management scripts to make release process easier
    To facilitate a proper release process we now have scripts to create
    complete tarballs in the fasion of autotools, which will also include
    the archived `inih` project. The net result is a completel tarball which
    build systems can use in a reproducable fashion without requiring network
    access to complete, thus improving security and ensuring consistency in
    results between various distributions should subprojects update.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ scripts/mkrelease.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Simple script to construct a redistributable and complete tarball of the
+# gamemode tree, including the git submodules, so that it can be trivially
+# packaged by distributions banning networking during build.
+#
+# Modified from Ikey Doherty's release scripts for use within
+# Feral Interactive's gamemode project.
+git submodule init
+git submodule update
+
+# Bump in tandem with meson.build, run script once new tag is up.
+VERSION="1.0"
+
+NAME="gamemode"
+./scripts/git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+xz -9 "${NAME}-${VERSION}.tar"
+
+# Automatically sign the tarball with GPG key of user running this script
+gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
