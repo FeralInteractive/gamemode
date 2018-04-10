8fb77e639d1c936f92f3e201d57f5fac91d592a6
Ikey Doherty <ikey@solus-project.com>
2018-04-10 14:04:07 +0100

    Ensure to set `_GNU_SOURCE` for POSIX compliance with signals
    The systemd bus headers aren't setting a POSIX compliance level so we
    define the catch-all to ensure the build doesn't error out, as seen with
    issue #3. This is due to the reliance of `siginfo_t`, which requires a
    minimum POSIX level of `199309L` when using glibc.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- lib/client_impl.c
+++ lib/client_impl.c
@@ -28,6 +28,9 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
  */
+
+#define _GNU_SOURCE
+
 #include <dlfcn.h>
 #include <errno.h>
 #include <stdio.h>
