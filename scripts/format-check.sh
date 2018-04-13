34e6d076a8bb6fce64fde8b48adc1ff36941d6ae
Marc Di Luzio <mdiluzio@feralinteractive.com>
2018-04-13 14:32:02 +0100

    Add a basic travis file for unit tests
      Only code formatting implemented at the moment.
    
      Travis is currently Ubuntu 14.04, which is missing multiple library versions needed. Bootstrapping in Python 3.5, Meson and Ninja is possible, but can take a while, but also bootstrapping in libsystemd is one step too many for now as it takes a significant amount of time.

---
+++ scripts/format-check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Simple script to check for clang-format compliance
+wget https://llvm.org/svn/llvm-project/cfe/trunk/tools/clang-format/git-clang-format
+chmod +x git-clang-format
+
+CLANG_FORMAT_OUTPUT=$(./git-clang-format --diff)
+if [[ ! $CLANG_FORMAT_OUTPUT == "no modified files to format" ]]; then
+  echo "Failed clang format check:"
+  echo "${CLANG_FORMAT_OUTPUT}"
+  exit 1
+else
+  echo "Passed clang format check"
+fi
+
