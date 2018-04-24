d42dd30e25049ff6b7a8306030a1bfa19fbe6697
Marc Di Luzio <mdiluzio@feralinteractive.com>
2018-04-24 17:09:06 +0100

    Fix format check

--- scripts/format-check.sh
+++ scripts/format-check.sh
@@ -4,7 +4,7 @@ wget https://llvm.org/svn/llvm-project/cfe/trunk/tools/clang-format/git-clang-fo
 chmod +x git-clang-format
 
 CLANG_FORMAT_OUTPUT=$(./git-clang-format HEAD^ HEAD --diff)
-if [[ ! $CLANG_FORMAT_OUTPUT == "no modified files to format" ]]; then
+if [[ ! ${CLANG_FORMAT_OUTPUT} == "no modified files to format" ]] && [[ ! -z ${CLANG_FORMAT_OUTPUT} ]]; then
   echo "Failed clang format check:"
   echo "${CLANG_FORMAT_OUTPUT}"
   exit 1
