403ce122f6fff7f619d2a81b9cb40d7a7715ab01
Kai Krakow <kai@kaishome.de>
2018-10-08 22:24:53 +0200

    refactor: Break ioprio API functions out of the main daemon source
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- daemon/gamemode.c
+++ daemon/gamemode.c
@@ -36,7 +36,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "governors-query.h"
 #include "governors.h"
 #include "helpers.h"
-#include "ioprio.h"
 #include "logging.h"
 
 #include <ctype.h>
@@ -157,56 +156,6 @@ void game_mode_context_destroy(GameModeContext *self)
 	config_destroy(self->config);
 
 	pthread_rwlock_destroy(&self->rwlock);
-}
-
-/**
- * Apply io priorities
- *
- * This tries to change the io priority of the client to a value specified
- * and can possibly reduce lags or latency when a game has to load assets
- * on demand.
- */
-static void game_mode_apply_ioprio(GameModeContext *self, pid_t client)
-{
-	LOG_MSG("Setting scheduling policies...\n");
-
-	/*
-	 * read configuration "ioprio" (0..7)
-	 */
-	int ioprio = 0;
-	config_get_ioprio_value(self->config, &ioprio);
-	if (IOPRIO_RESET_DEFAULT == ioprio) {
-		LOG_MSG("IO priority will be reset to default behavior (based on CPU priority).\n");
-		ioprio = 0;
-	} else if (IOPRIO_DONT_SET == ioprio) {
-		return;
-	} else {
-		/* maybe clamp the value */
-		int invalid_ioprio = ioprio;
-		ioprio = CLAMP(0, 7, ioprio);
-		if (ioprio != invalid_ioprio)
-			LOG_ERROR("IO priority value %d invalid, clamping to %d\n", invalid_ioprio, ioprio);
-
-		/* We support only IOPRIO_CLASS_BE as IOPRIO_CLASS_RT required CAP_SYS_ADMIN */
-		ioprio = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_BE, ioprio);
-	}
-
-	/*
-	 * Actually apply the io priority
-	 */
-	int c = IOPRIO_PRIO_CLASS(ioprio), p = IOPRIO_PRIO_DATA(ioprio);
-	if (ioprio_set(IOPRIO_WHO_PROCESS, client, ioprio) == 0) {
-		if (0 == ioprio)
-			LOG_MSG("Resetting client [%d] IO priority.\n", client);
-		else
-			LOG_MSG("Setting client [%d] IO priority to (%d,%d).\n", client, c, p);
-	} else {
-		LOG_ERROR("Setting client [%d] IO priority to (%d,%d) failed with error %d, ignoring\n",
-		          client,
-		          c,
-		          p,
-		          errno);
-	}
 }
 
 /**
