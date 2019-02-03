6b268e8349956ed7a0bc4a4d4efda316d604d211
Marc Di Luzio <marc.diluzio@gmail.com>
2019-02-03 15:54:29 +0000

    Convert "apply_gpu_optimisations" to a string with a special key

--- daemon/daemon_config.h
+++ daemon/daemon_config.h
@@ -133,7 +133,7 @@ void config_get_ioprio_value(GameModeConfig *self, int *value);
 /*
  * Get various config info for gpu optimisations
  */
-void config_get_apply_gpu_optimisations(GameModeConfig *self, long *value);
+void config_get_apply_gpu_optimisations(GameModeConfig *self, char value[CONFIG_VALUE_MAX]);
 void config_get_gpu_vendor(GameModeConfig *self, long *value);
 void config_get_gpu_device(GameModeConfig *self, long *value);
 void config_get_nv_core_clock_mhz_offset(GameModeConfig *self, long *value);
