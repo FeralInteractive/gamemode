0bccdd5d69e5f9f0bd7db7d20b8d095c3efc747e
Marc Di Luzio <marc.diluzio@gmail.com>
2019-02-03 17:36:08 +0000

    Move the TODO into the control file

--- daemon/gpu-control.c
+++ daemon/gpu-control.c
@@ -34,6 +34,13 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "gpu-control.h"
 #include "logging.h"
 
+// TODO
+// Gather GPU type and information automatically
+// Apply Nvidia GPU settings (CoolBits will be needed)
+// Apply AMD GPU settings (Will need user changing pwm1_enable)
+// Intel?
+// Provide documentation on optimisations
+
 // NVIDIA
 // Running these commands:
 // nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1400'
